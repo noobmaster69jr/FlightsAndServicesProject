3c707dd32e0c5bb7bf6c1fc1e4e2d2ac6f079bbd
noobmaster69jr <srashwin22@gmail.com>
2023-04-01 07:29:54 +0530

    Completed City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -7,6 +7,7 @@ class CityRepository{
         const city = await City.create({name});
         return city
     }catch(error){
+        console.log("Something went wrong in the repository layer")
         throw {error};
     }
    }
@@ -18,8 +19,32 @@ class CityRepository{
                 id: cityId
             }
         })
+        return true
     }catch(error){
+        console.log("Something went wrong in the repository layer")
         throw (error);
+    }
+   }
+
+   async updateCity({cityId, data}){
+    try {
+        const city = await City.update(data, {
+            where: {
+                id: cityId
+            }
+        })
+        return city
+    } catch (error) {
+        
+    }
+   }
+
+   async getCity({cityId}){
+    try {
+        const city = await City.findByPk(cityId)
+    } catch (error) {
+        console.log("something went wrong in the repository layer")
+        throw(error)
     }
    }
 }

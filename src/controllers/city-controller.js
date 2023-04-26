cee82af7001342df038e48b2e90b6114484d6ab1
noobmaster69jr <srashwin22@gmail.com>
2023-04-26 12:44:32 +0530

    Added get all cities api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -81,9 +81,29 @@ const update = async (req, res) => {
   }
 };
 
+const getAll = async (req, res) =>{
+  try {
+    const cities = await cityService.getAllCities();
+     return res.status(200).json({
+       data: response,
+       success: true,
+       message: "Successfully updated the city",
+       err: {},
+     });
+  } catch (error) {
+     console.log(error);
+     return res.status(500).json({
+       data: {},
+       success: false,
+       message: "Not able to fetch the city",
+       err: error,
+     });
+  }
+}
 module.exports = {
   create,
   destroy,
   get,
+  getAll, 
   update,
 };
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -51,6 +51,16 @@ class CityRepository {
       throw error;
     }
   }
+
+  async getAllCities(){
+    try{
+      const cities = await City.findAlll();
+      return cities;
+    }catch(error){
+      console.log("something went wrong int the repository layer");
+      throw(error);
+    }
+  }
 }
 
 module.exports = CityRepository;
